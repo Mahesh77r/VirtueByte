b736ff3661ae80ebbda6d89ed8846739f0965b99
Spandan Jagtap <spandanjagtap07@gmail.com>
2024-07-03 17:19:24 +0530

    updated

--- client/src/Pages/spandan/Spandan.js
+++ client/src/Pages/spandan/Spandan.js
@@ -5,15 +5,15 @@ const Spandan = () => {
     <div className='Contact'>
         <h1>Contact Us</h1>
       <div className='Mahesh'>
-        <h2>Name: Mahesh Rohane<br></br>
+        <h2 className='details'>Name: Mahesh Rohane<br></br>
         Phone no: 123456789</h2>
       </div>
       <div className='Spandan'>
-        <h2>Name: Spandan Jagtap<br></br>
+        <h2 className='details'>Name: Spandan Jagtap<br></br>
         Phone no: 987654321</h2>
       </div>
       <div className='Laxminarayan'>
-        <h2>Name: Laxminarayan Gundale<br></br>
+        <h2 className='details'>Name: Laxminarayan Gundale<br></br>
         Phone no: 109876543</h2>
       </div>
     </div>
