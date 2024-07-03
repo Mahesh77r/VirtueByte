152ee8efafead7460dc5fb90e167f40c3db8f4e9
Spandan Jagtap <spandanjagtap07@gmail.com>
2024-07-03 13:12:55 +0530

    install packages

---
+++ client/src/Pages/Spandan.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Spandan = () => {
+  return (
+    <div className='Contact'>
+        <h1>Contact Us</h1>
+      <div className='Mahesh'>
+        <h2>Name: Mahesh
+        Phone no: 123456789</h2>
+      </div>
+      <div className='Spandan'>
+        <h2>Name: Spandan
+        Phone no: 987654321</h2>
+      </div>
+      <div className='Laxminarayan'>
+        <h2>Name: Laxminarayan
+        Phone no: 109876543</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Spandan
