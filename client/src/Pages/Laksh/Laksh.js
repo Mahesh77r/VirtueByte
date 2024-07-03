400081658d3b0dc1817614749546d304d3590190
laxminarayangundale <lakshpatil96k@gmail.com>
2024-07-03 13:53:27 +0530

    hello

---
+++ client/src/Pages/Laksh/Laksh.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Laksh.css'
+
+const Laksh = () => {
+  return (
+    <div className='about-us-container'>
+        <div className='about-us-contents' >
+           <h1>ABOUT US</h1>
+           <h5>For more than 7 years, we have provided clients with technology services such as software development, cross-platform mobile app, native app development, and QA services. We have created 60+ customized solutions, including enterprise software, e-commerce, B2B, IoT, and big data platforms, while working with 28+ clients. We meet deadlines and adhere to budgets while delivering top-notch, reliable solutions that help clients' businesses go digital in accordance with their preferred technology.</h5>
+        </div>
+        <div className='about-us-image'>
+        <img src='about-us.svg' alt='' />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Laksh
